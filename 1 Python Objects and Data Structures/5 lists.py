0f622c7e6a91b2b3860174e250d95b57c00e23fc
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-09 12:20:39 +0530

    Lists in Python

---
+++ 1 Python Objects and Data Structures/5 lists.py
@@ -0,0 +1,5 @@
+# List are ordered sequence that can hold a variety of objects
+# They use [] and commas to separate objects in the list
+# Lists support indexing and slicing 
+# Lists can be nested and also have a variety of useful methods that can be called off of them
+
