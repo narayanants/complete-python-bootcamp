b9eccf8729cdc37a57ddb22c44fbfa4330962a6a
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-09 13:47:02 +0530

    Tuples in Python

---
+++ 1 Python Objects and Data Structures/7 tuples.py
@@ -0,0 +1,20 @@
+# Tuples
+# Similar to lists
+# They are immutable
+# Once an element is assigned to a tuple it cannot be reassigned
+# Tuples use parenthesis (1,2,3)
+
+
+
+t = (1,2,3)
+print(type(t)) #<class 'tuple'>
+print(len(t))#3
+
+print(t[0]) # 1
+print(t[-1]) # 3
+
+t = ('a','a','b')
+print(t.count('a')) #2
+print(t.index('a')) #0
+print(t.index('b')) # 2
+
