a000c9405eb1c19a6f3eb12b6d892f456964ddc1
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-09 11:45:26 +0530

    string methods and properties

---
+++ 1 Python Objects and Data Structures/string_properties_and_methods.py
@@ -0,0 +1,19 @@
+name = 'Narayanan'
+
+last_letters = name[1:]
+print(last_letters) #arayanan
+
+total_name = 'N' + last_letters
+print(total_name)
+
+letter = 'z'; # zzzzzzzzzz
+print(letter * 10)
+
+letters = '2' + '3'
+print(letters) # 23
+
+x = 'Hello world'
+print(x.upper()) # HELLO WORLD
+
+x = 'This is a string'
+print(x.split('i')) # ['Th', 's ', 's a str', 'ng']
