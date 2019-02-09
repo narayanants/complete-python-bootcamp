ce33e367cb6dfc4632eb48ab3c840a33470d5e96
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-09 11:45:17 +0530

    Numbers

---
+++ 1 Python Objects and Data Structures/2 Numbers.py
@@ -0,0 +1,10 @@
+mystring = 'abcdefghijklmnopq';
+print(mystring[-3])
+
+print(mystring[2:])
+print(mystring[0:3])
+print(mystring[3:6])
+print(mystring[1:3])
+print(mystring[3:15])
+print(mystring[::]) # Returns from begining to end of the string
+print(mystring[::3]) # Jump Letters
