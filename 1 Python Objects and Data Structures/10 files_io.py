316f40e85603b0679109d43face5dcd6eb31b064
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-12 18:48:08 +0530

    files_io

---
+++ 1 Python Objects and Data Structures/10 files_io.py
@@ -0,0 +1,16 @@
+'''
+    IO with Text Files:
+    ------------------
+
+    r = read 
+    w = write
+    a = append
+
+'''
+
+with open('abcd.txt','w') as a:
+    a.write('This is the First line \nThis is the second line \nThis is the third line')
+
+with open('abcd.txt','r') as f:
+    contents = f.read()
+    print(contents)
