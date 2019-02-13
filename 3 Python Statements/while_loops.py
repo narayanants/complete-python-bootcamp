75e0406fdb168914f4a8496e7ae371d77bcdad4b
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-13 17:27:00 +0530

    while loops

---
+++ 3 Python Statements/while_loops.py
@@ -0,0 +1,19 @@
+'''
+    While Loops:
+    ============
+
+    * While some condition remains true
+
+'''
+
+
+x = [1, 2, 3]
+for item in x:
+    pass
+
+
+mystring = "Narayanan"
+for letter in mystring:
+    if letter == 'a':
+        break
+    print(letter)
