c979c73413a40a752ec576295def13caf848f9da
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-13 17:26:54 +0530

    operators

---
+++ 3 Python Statements/operators.py
@@ -0,0 +1,46 @@
+from random import randint
+from random import shuffle
+'''
+Useful Operators
+=================
+
+
+'''
+
+for num in range(0, 11, 2):
+    print(num)
+
+print(list(range(0, 11, 2)))
+
+index_count = 0
+for letter in 'ABCDE':
+    print('At index {} the letter is {}'.format(index_count, letter))
+
+word = "ABCDE"
+for index, letter in enumerate(word):
+    print(index)
+    print(letter)
+
+mylist1 = [1, 2, 3]
+mylist2 = ['a', 'b', 'c']
+mylist3 = [100, 200, 300]
+for item in zip(mylist1, mylist2, mylist3):
+    print(item)
+
+
+print(2 in [2, 3, 4])
+
+
+mylist = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(min(mylist))
+print(max(mylist))
+
+
+mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+shuffle(mylist)
+print(mylist)
+
+print(randint(0, 100))
+
+
+letter_input = input('Enter a number')
