21baa6f5a17c49badf4f3962a2cfcd68aa5c38af
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-13 17:25:17 +0530

    elif_elseif

---
+++ 3 Python Statements/elif_elseif.py
@@ -0,0 +1,22 @@
+'''
+    Elif and Else-if statements in Python
+    ======================================
+
+    Control Flow:
+    -------------
+
+    if (some_condition):
+        #execute_some_code
+    elif some_other_condition:
+        #do_something_different
+    else:
+        #do_something
+'''
+
+name = "Narayanan"
+if name == "Gokul":
+    print('Hello Gokul')
+elif name == "Deepak":
+    print("Hello Deepak")
+else:
+    print('Hello Gokul')
