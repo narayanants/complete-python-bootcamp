20d1d32b14ffecb58ff149915bd34f5042a071e0
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-14 11:14:17 +0530

    formatting

--- 1 Python Objects and Data Structures/8 sets.py
+++ 1 Python Objects and Data Structures/8 sets.py
@@ -5,14 +5,15 @@
 
 
 myset = set()
-print(myset) #set()
+print(myset)  # set()
 
 myset.add(1)
-print(myset) #{1}
+print(myset)  # {1}
 
 myset.add(2)
-print(myset) #{1, 2}
+print(myset)  # {1, 2}
 
 
-mylist = [1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,5,5,5,5,]
-print(set(mylist)) # {1, 2, 3, 4, 5}
+mylist = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3,
+          3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, ]
+print(set(mylist))  # {1, 2, 3, 4, 5}
