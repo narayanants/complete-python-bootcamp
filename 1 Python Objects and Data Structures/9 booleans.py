f7310c065ac9e7985b97020a596759307a9254ee
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-14 11:14:21 +0530

    formatting

--- 1 Python Objects and Data Structures/9 booleans.py
+++ 1 Python Objects and Data Structures/9 booleans.py
@@ -3,8 +3,8 @@
 # Booleans are operators that allow you to convey True or False statements
 # They are very important later on when we deal with logic and control flow
 
-type(False) # Bool
-1 == 1 # True
+type(False)  # Bool
+1 == 1  # True
 
 b = None
-print(b) 
+print(b)
