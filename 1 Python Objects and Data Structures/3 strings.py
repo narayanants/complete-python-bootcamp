8d878efbf082716c0096fe8f5ba6c9f95aa49e6f
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-14 11:14:04 +0530

    formatting

--- 1 Python Objects and Data Structures/3 strings.py
+++ 1 Python Objects and Data Structures/3 strings.py
@@ -6,25 +6,22 @@
 # Slicing allows you to grab a subsection of multiple characters
 # Strings are immutable
 
-
 name = 'Narayanan'
 last_letters = name[1:]
-print(last_letters) #arayanan
+print(last_letters)  # arayanan
 
 total_name = 'N' + last_letters
 print(total_name)
 
-letter = 'z'; # zzzzzzzzzz
+letter = 'z'  # zzzzzzzzzz
 print(letter * 10)
 
 letters = '2' + '3'
-print(letters) # 23
+print(letters)  # 23
 
 x = 'Hello world'
-print(x.upper()) # HELLO WORLD
-print(x.lower()) # hello world
+print(x.upper())  # HELLO WORLD
+print(x.lower())  # hello world
 
 x = 'This is a string'
-print(x.split('i')) # ['Th', 's ', 's a str', 'ng']
-
-
+print(x.split('i'))  # ['Th', 's ', 's a str', 'ng']
