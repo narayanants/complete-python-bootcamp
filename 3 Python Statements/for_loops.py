79c19b81cacadbb66dc37d87db3de2f08c3874a6
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-14 12:57:13 +0530

    for loops

--- 3 Python Statements/for_loops.py
+++ 3 Python Statements/for_loops.py
@@ -10,7 +10,6 @@ for num in mylist:
     else:
         print(f'Odd Number {num}')
 
-
 mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 list_sum = 0
 for num in mylist:
@@ -21,7 +20,6 @@ print(list_sum)
 mylist = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
 for a, b, c in mylist:
     print(a)
-
 
 d = {'k1': 1, 'k2': 2, 'k3': 3}
 for k, v in d.items():
