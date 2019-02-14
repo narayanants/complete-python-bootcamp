b86628d282b17729c5ed768440be3f69a298933b
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-14 12:57:09 +0530

    elif elseif

--- 3 Python Statements/elif_elseif.py
+++ 3 Python Statements/elif_elseif.py
@@ -20,3 +20,10 @@ elif name == "Deepak":
     print("Hello Deepak")
 else:
     print('Hello Gokul')
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+sum = 0
+for val in numbers:
+    sum = sum + val
+print("The sum is", sum)
