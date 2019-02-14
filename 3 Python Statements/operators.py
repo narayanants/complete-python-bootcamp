4028cb6e585c449a6a97fd07c22a4c704b2d7803
Narayanan Ts <tsnarayanan7@gmail.com>
2019-02-14 12:57:17 +0530

    operators

--- 3 Python Statements/operators.py
+++ 3 Python Statements/operators.py
@@ -1,14 +1,21 @@
-from random import randint
 from random import shuffle
+from random import randint
 '''
 Useful Operators
 =================
 
+    * range(0,11,2)
+    * enumerate()
+    * zip
+    * min
+    * max
+    * randint
+    * input
 
 '''
-
 for num in range(0, 11, 2):
     print(num)
+
 
 print(list(range(0, 11, 2)))
 
@@ -34,7 +41,6 @@ print(2 in [2, 3, 4])
 mylist = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 print(min(mylist))
 print(max(mylist))
-
 
 mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 shuffle(mylist)
